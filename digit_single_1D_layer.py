79adf90e99464b398157dcd518d3988438584ac7
Nicolas Girot <ngirot@hotmail.com>
2016-11-18 22:55:00 +0100

    Rename main script

--- digit_single_1D_layer.py
+++ digit_single_1D_layer.py
@@ -4,6 +4,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import tensorflow as tf
 
 # Init
+print 'Initialisation'
 x = tf.placeholder(tf.float32, [None, 784])
 
 W = tf.Variable(tf.zeros([784, 10]))
@@ -12,6 +13,7 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Training
+print 'Training'
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
@@ -27,6 +29,7 @@ for i in range(1000):
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # Evaluation
+print 'Evaluation'
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
